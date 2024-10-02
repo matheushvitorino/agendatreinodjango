da2f542bccbb5fd6391fa18ea3d1d332d57637a3
Matheus <matheushenriquevitorino@gmail.com>
2024-10-02 16:50:47 -0300

    edição do models

--- treino/admin.py
+++ treino/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-from .models import TipoTreino,Treino,Exercicio
-
+from .models import TipoTreino,Treino,Exercicio,TreinoExercicio
 
 class TipoTreinoAdmin(admin.ModelAdmin):
     list_display=('nome',)
     search_fields=('nome',)
 
+class TreinoExercicioInline(admin.TabularInline):
+    model = TreinoExercicio
+
 class ExercicioAdmin(admin.ModelAdmin):
-    list_display=('nome',)
+    list_display=('nome','tipo')
     search_fields=('nome',)
     
 class TreinoAdmin(admin.ModelAdmin):
-    list_display=('tipo','series','repeticoes')
+    list_display=('tipo',)
     search_fields=('tipo','exercicio')
-    filter_horizontal=('exercicio',)
-
+    inlines = [TreinoExercicioInline]
+    
 admin.site.register(TipoTreino, TipoTreinoAdmin)
 admin.site.register(Exercicio, ExercicioAdmin)
 admin.site.register(Treino, TreinoAdmin)
--- treino/models.py
+++ treino/models.py
@@ -10,25 +10,27 @@ class TipoTreino(models.Model):
 
 class Exercicio(models.Model):
     nome = models.CharField(max_length=100)
-    tipo = models.OneToOneField(TipoTreino, on_delete=models.PROTECT)
+    tipo = models.ForeignKey(TipoTreino, on_delete=models.PROTECT)
     
     def __str__(self):
         return f'{self.nome} de {self.tipo}'
     
     
 class Treino(models.Model):
-    series ={
-        '3':3,
-        '4':4,
-        '5':4,
-        '6':6,
-        }
-    exercicio = models.ManyToManyField(Exercicio, related_name='treinos' )
+
+    exercicio = models.ManyToManyField(Exercicio, related_name='treinos',through='TreinoExercicio' )
     tipo = models.OneToOneField(TipoTreino, on_delete=models.PROTECT)
-    series = models.IntegerField(choices=series, default='3')
-    repeticoes = models.IntegerField()
     criado = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f'Treino de {self.tipo}'
+    
+class TreinoExercicio(models.Model):
+    treino = models.ForeignKey(Treino, on_delete=models.CASCADE)
+    exercicio = models.ForeignKey(Exercicio, on_delete=models.CASCADE)
+    series = models.IntegerField(choices=[(3, 3), (4, 4), (5, 5), (6, 6)], default=3)
+    repeticoes = models.IntegerField()
+    
+    def __str__(self):
+        return f'Exercicio de {self.exercicio} {self.series}x{self.repeticoes}'
     
